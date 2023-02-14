166251320b1c626aa7af7ff599d511db8672456e
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-02-14 08:13:28 +0700

    Add project files.

---
+++ SaleManagerment_WPF/Entity/Member.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaleManagerment_WPF.Entity
+{
+    internal class Member
+    {
+        public int MemberId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+
+    }
+}
